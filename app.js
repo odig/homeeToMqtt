44840979f12ec576533f7ae1d9cb6e01b5940452
bwelker <bwelker@gmail.com>
2018-03-20 20:16:17 +0100

    Winston Error

--- app.js
+++ app.js
@@ -70,7 +70,6 @@ if (config.identifierBool == null) config.identifierBool = 'devices/bool/'
 const winston = require('winston');
 const logger = winston.createLogger({
     level: 'info',
-    format: winston.format.json(),
     transports: [
       //
       // - Write to all logs with level `info` and below to `combined.log` 
